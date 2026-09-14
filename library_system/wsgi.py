594ffb806516f09e29f9d81592d11703a5a7974b
harishma2122 <rishjune2207@gmail.com>
2026-09-14 22:12:29 +0530

    Expose app instance in wsgi.py for Vercel

--- library_system/wsgi.py
+++ library_system/wsgi.py
@@ -2,5 +2,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_system.settings')
-app = get_wsgi_application()
-application = app
+
+application = get_wsgi_application()
+
+# ADD THIS LINE:
+app = application
